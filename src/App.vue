78c5dd81820439aea4c7f0b37463f4703130ab70
Mohammad Agbareya <muhammed@irestweb.com>
2023-01-27 00:13:50 +0200

    dev

--- src/App.vue
+++ src/App.vue
@@ -1,181 +1,201 @@
-<script>
-const randomInt = (max = 9, min = 1) => Math.floor(Math.random() * max) + 1;
+<script lang="ts">
+interface Cell {
+    num: number;
+    selected: boolean;
+    ans: boolean;
+    off: boolean;
+}
+
+interface State {
+    x: number;
+    y: number;
+    table: Cell[][];
+    rowsAns: number[];
+    colsAns: number[];
+    xyOptions: number[];
+}
+
+const randomInt = (max = 9, min = 1): number => Math.floor(Math.random() * max) + min;
 
 export default {
-  data() {
-    return {
-      x: 4,
-      y: 4,
-      table: [],
-      rowsAns: [],
-      colsAns: [],
-    };
-  },
-  methods: {
-    reset() {
-      this.table = Array(this.y);
+    data(): State {
+        return {
+            x: 4,
+            y: 4,
+            table: [],
+            rowsAns: [],
+            colsAns: [],
+            xyOptions: [3, 4, 5, 6],
+        };
+    },
+    methods: {
+        reset() {
+            this.table = Array(this.y);
 
-      for (let i = 0; i < this.y; i++) {
-        this.table[i] = Array(this.x);
+            for (let i = 0; i < this.y; i++) {
+                this.table[i] = Array(this.x);
 
-        for (let j = 0; j < this.x; j++) {
-          this.table[i][j] = {
-            num: randomInt(),
-            selected: false,
-            off: false,
-            ans: randomInt(this.x) > this.x / 2,
-          };
-        }
+                for (let j = 0; j < this.x; j++) {
+                    this.table[i][j] = {
+                        num: randomInt(),
+                        selected: false,
+                        off: false,
+                        ans: randomInt(this.x) > this.x / 2,
+                    };
+                }
 
-        if (this.table[i].every((a) => !a.ans)) {
-          this.table[i][randomInt(this.x - 1, 0)].ans = true;
-        }
-      }
+                if (this.table[i].every((a) => !a.ans)) {
+                    this.table[i][randomInt(this.x - 1, 0)].ans = true;
+                }
+            }
 
-      for (let i = 0; i < this.x; i++) {
-        let flag = false;
+            for (let i = 0; i < this.x; i++) {
+                let flag = false;
 
-        for (let j = 0; j < this.y; j++) {
-          flag ||= this.table[j][i].ans;
-        }
+                for (let j = 0; j < this.y; j++) {
+                    flag ||= this.table[j][i].ans;
+                }
 
-        if (!flag) {
-          this.table[randomInt(this.y - 1, 0)][i].ans = true;
-        }
-      }
+                if (!flag) {
+                    this.table[randomInt(this.y - 1, 0)][i].ans = true;
+                }
+            }
 
-      this.rowsAns = this.calcRowsSum("ans");
-      this.colsAns = this.calcColsSum("ans");
+            this.rowsAns = this.calcRowsSum("ans");
+            this.colsAns = this.calcColsSum("ans");
+        },
+        calcRowsSum(key = "selected") {
+            return this.table.map((row) => row.reduce((a, b) => (a += b?.[key] && b?.num), 0));
+        },
+        calcColsSum(key = "selected") {
+            return this.table.reduce((r: number[], a: Cell[]) => {
+                a.forEach((b, i) => {
+                    r[i] ||= 0;
+                    r[i] += +b?.[key] && b.num;
+                });
+
+                return r;
+            }, []);
+        },
+        clear() {
+            this.table.forEach((row) => row.forEach((cell) => (cell.selected = false)));
+        },
+        toggleSelected(cell) {
+            !cell.off && (cell.selected = !cell.selected);
+        },
+        toggleOff(cell) {
+            !cell.selected && (cell.off = !cell.off);
+        },
+        rowUnselectedOff(i: number) {
+            this.table[i].forEach((cell: Cell) => !cell.selected && (cell.off = true));
+        },
+        colUnselectedOff(j: number) {
+            for (let i = 0; i < this.y; i++) {
+                !this.table[i][j].selected && (this.table[i][j].off = true);
+            }
+        },
     },
-    calcRowsSum(key = "selected") {
-      return this.table.map((row) =>
-        row.reduce((a, b) => (a += b?.[key] && b?.num), 0)
-      );
+    computed: {
+        won() {
+            return (
+                this.rowsSum.join("") == this.rowsAns.join("") &&
+                this.colsSum.join("") == this.colsAns.join("")
+            );
+        },
+        rowsSum() {
+            return this.calcRowsSum();
+        },
+        colsSum() {
+            return this.calcColsSum();
+        },
+        // rowsAns() {
+        //   return this.calcRowsSum("ans");
+        // },
+        // colsAns() {
+        //   return this.calcColsSum("ans");
+        // },
     },
-    calcColsSum(key = "selected") {
-      return this.table.reduce((r, a) => {
-        a.forEach((b, i) => {
-          r[i] ||= 0;
-          r[i] += +b?.[key] && b.num;
-        });
-        return r;
-      }, []);
+    created() {
+        this.reset();
     },
-    clear() {
-      this.table.forEach((row) =>
-        row.forEach((cell) => (cell.selected = false))
-      );
+    watch: {
+        won(val) {
+            if (val) {
+                this.$confetti.start();
+            } else {
+                this.$confetti.stop();
+            }
+        },
     },
-    toggleSelected(cell) {
-      if (!cell.off) cell.selected = !cell.selected;
-    },
-    toggleOff(cell) {
-      if (!cell.selected) cell.off = !cell.off;
-    },
-  },
-  computed: {
-    won() {
-      return (
-        this.rowsSum.join("") == this.rowsAns.join("") &&
-        this.colsSum.join("") == this.colsAns.join("")
-      );
-    },
-    rowsSum() {
-      return this.calcRowsSum();
-    },
-    colsSum() {
-      return this.calcColsSum();
-    },
-    // rowsAns() {
-    //   return this.calcRowsSum("ans");
-    // },
-    // colsAns() {
-    //   return this.calcColsSum("ans");
-    // },
-  },
-  created() {
-    this.reset();
-  },
-  watch: {
-    won(val) {
-      if (val) {
-        this.$confetti.start();
-      } else {
-        this.$confetti.stop();
-      }
-    },
-  },
 };
 </script>
 
 <style>
 * {
-  text-align: center !important;
+    text-align: center !important;
 }
 
 .x {
-  align-self: center;
-  margin-bottom: var(--spacing);
+    align-self: center;
+    margin-bottom: var(--spacing);
 }
 
 td {
-  cursor: pointer;
+    cursor: pointer;
 }
 
 .selected {
-  background-color: #9bc362;
-  color: #000000;
+    background-color: #9bc362;
+    color: #000000;
 }
 
 .done {
-  background-color: gold;
-  color: #000000;
-  font-weight: bold;
+    background-color: gold;
+    color: #000000;
+    font-weight: bold;
 }
 
 .off {
-  color: var(--muted-color);
+    color: var(--muted-color);
 }
 </style>
 
 <template>
-  <div class="grid">
-    <select v-model="x">
-      <option :value="3">3</option>
-      <option :value="4">4</option>
-      <option :value="5">5</option>
-      <option :value="6">6</option>
-    </select>
-    <span class="x">X</span>
-    <select v-model="y">
-      <option :value="3">3</option>
-      <option :value="4">4</option>
-      <option :value="5">5</option>
-      <option :value="6">6</option>
-    </select>
-    <button @click="reset">Shuffle</button>
-  </div>
-  <br /><br /><br />
-  <table>
-    <tr v-for="(row, i) in table">
-      <td
-        v-for="(cell, j) in row"
-        :class="{ selected: cell.selected, off: cell.off }"
-        @click="toggleSelected(cell)"
-        @dblclick="toggleOff(cell)"
-      >
-        {{ cell.num }}
-      </td>
-      <td :class="{ done: rowsAns[i] == rowsSum[i] }">
-        {{ rowsAns[i] }} ({{ rowsSum[i] }})
-      </td>
-    </tr>
-    <tr>
-      <td v-for="(num, i) in colsAns" :class="{ done: num == colsSum[i] }">
-        {{ num }} ({{ colsSum[i] }})
-      </td>
-    </tr>
-  </table>
-  <br /><br />
-  <button @click="clear">Clear</button>
+    <div class="grid">
+        <select v-model="x">
+            <option v-for="opt in xyOptions" :value="opt">{{ opt }}</option>
+        </select>
+        <span class="x">X</span>
+        <select v-model="y">
+            <option v-for="opt in xyOptions" :value="opt">{{ opt }}</option>
+        </select>
+        <button @click="reset">Shuffle</button>
+    </div>
+    <br /><br /><br />
+    <table>
+        <tr v-for="(row, i) in table">
+            <td
+                v-for="(cell, j) in row"
+                :class="{ selected: cell.selected, off: cell.off }"
+                @click="toggleSelected(cell)"
+                @dblclick="toggleOff(cell)"
+            >
+                {{ cell.num }}
+            </td>
+            <td :class="{ done: rowsAns[i] == rowsSum[i] }" @click="rowUnselectedOff(i)">
+                {{ rowsAns[i] }} ({{ rowsSum[i] }})
+            </td>
+        </tr>
+        <tr>
+            <td
+                v-for="(num, j) in colsAns"
+                :class="{ done: num == colsSum[j] }"
+                @click="colUnselectedOff(j)"
+            >
+                {{ num }} ({{ colsSum[j] }})
+            </td>
+        </tr>
+    </table>
+    <br /><br />
+    <button @click="clear">Clear</button>
 </template>
