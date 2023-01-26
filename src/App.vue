0c606c7154a42ae1baebb20d35a6ad4b869a6a0c
mohammadhosry <mohammad.hosry@gmail.com>
2023-01-26 10:23:42 +0200

    div

--- src/App.vue
+++ src/App.vue
@@ -2,91 +2,97 @@
 const randomInt = (max = 9, min = 1) => Math.floor(Math.random() * max) + 1;
 
 export default {
-    data() {
-        return {
-            x: 4,
-            y: 4,
-            table: [],
-        };
+  data() {
+    return {
+      x: 4,
+      y: 4,
+      table: [],
+      rowsAns: [],
+      colsAns: [],
+    };
+  },
+  methods: {
+    reset() {
+      this.table = Array(this.y);
+
+      for (let i = 0; i < this.y; i++) {
+        this.table[i] = Array(this.x);
+
+        for (let j = 0; j < this.x; j++) {
+          this.table[i][j] = {
+            num: randomInt(),
+            selected: false,
+            ans: randomInt(this.x) > this.x / 2,
+          };
+        }
+
+        if (this.table[i].every((a) => !a.ans)) {
+          this.table[i][randomInt(this.x - 1, 0)].ans = true;
+        }
+      }
+
+      for (let i = 0; i < this.x; i++) {
+        let flag = false;
+
+        for (let j = 0; j < this.y; j++) {
+          flag ||= this.table[j][i].ans;
+        }
+
+        if (!flag) {
+          this.table[randomInt(this.y - 1, 0)][i].ans = true;
+        }
+      }
+
+      this.rowsAns = this.calcRowsSum("ans");
+      this.colsAns = this.calcColsSum("ans");
     },
-    methods: {
-        reset() {
-            this.table = Array(this.y);
-
-            for (let i = 0; i < this.y; i++) {
-                this.table[i] = Array(this.x);
-
-                for (let j = 0; j < this.x; j++) {
-                    this.table[i][j] = {
-                        num: randomInt(),
-                        selected: false,
-                        ans: randomInt(this.x) > this.x / 2,
-                    };
-                }
-
-                if (this.table[i].every((a) => !a.ans)) {
-                    this.table[i][randomInt(this.x - 1, 0)].ans = true;
-                }
-            }
-
-            for (let i = 0; i < this.x; i++) {
-                let flag = false;
-
-                for (let j = 0; j < this.y; j++) {
-                    flag ||= this.table[j][i].ans;
-                }
-
-                if (!flag) {
-                    console.log(i);
-                    this.table[randomInt(this.y - 1, 0)][i].ans = true;
-                }
-            }
-        },
-        calcRowsSum(key = "selected") {
-            return this.table.map((row) => row.reduce((a, b) => (a += b?.[key] && b?.num), 0));
-        },
-        calcColsSum(key = "selected") {
-            return this.table.reduce((r, a) => {
-                a.forEach((b, i) => {
-                    r[i] ||= 0;
-                    r[i] += +b?.[key] && b.num;
-                });
-                return r;
-            }, []);
-        },
+    calcRowsSum(key = "selected") {
+      return this.table.map((row) =>
+        row.reduce((a, b) => (a += b?.[key] && b?.num), 0)
+      );
     },
-    computed: {
-        won() {
-            return (
-                this.rowsSum.join("") == this.rowsAns.join("") &&
-                this.colsSum.join("") == this.colsAns.join("")
-            );
-        },
-        rowsSum() {
-            return this.calcRowsSum();
-        },
-        colsSum() {
-            return this.calcColsSum();
-        },
-        rowsAns() {
-            return this.calcRowsSum("ans");
-        },
-        colsAns() {
-            return this.calcColsSum("ans");
-        },
+    calcColsSum(key = "selected") {
+      return this.table.reduce((r, a) => {
+        a.forEach((b, i) => {
+          r[i] ||= 0;
+          r[i] += +b?.[key] && b.num;
+        });
+        return r;
+      }, []);
     },
-    created() {
-        this.reset();
+  },
+  computed: {
+    won() {
+      return (
+        this.rowsSum.join("") == this.rowsAns.join("") &&
+        this.colsSum.join("") == this.colsAns.join("")
+      );
     },
-    watch: {
-        won(val) {
-            if (val) {
-                this.$confetti.start();
-            } else {
-                this.$confetti.stop();
-            }
-        },
+    rowsSum() {
+      return this.calcRowsSum();
     },
+    colsSum() {
+      return this.calcColsSum();
+    },
+    // rowsAns() {
+    //   return this.calcRowsSum("ans");
+    // },
+    // colsAns() {
+    //   return this.calcColsSum("ans");
+    // },
+  },
+  created() {
+    this.reset();
+  },
+  watch: {
+    won(val) {
+      if (val) {
+        this.$confetti.start();
+      } else {
+        this.$confetti.stop();
+      }
+    },
+  },
 };
 </script>
 
@@ -109,24 +115,40 @@ td {
 </style>
 
 <template>
+  <div class="grid">
+    <select v-model="x">
+      <option :value="3">3</option>
+      <option :value="4">4</option>
+      <option :value="5">5</option>
+      <option :value="6">6</option>
+    </select>
+    X
+    <select v-model="y">
+      <option :value="3">3</option>
+      <option :value="4">4</option>
+      <option :value="5">5</option>
+      <option :value="6">6</option>
+    </select>
     <button @click="reset">reset</button>
-    <!-- {{ table }} -->
-    <br /><br /><br />
-    <table>
-        <tr v-for="(row, i) in table">
-            <td
-                v-for="(cell, j) in row"
-                :class="{ selected: cell.selected }"
-                @click="table[i][j].selected = !cell.selected"
-            >
-                {{ cell.num }}
-            </td>
-            <td :class="{ done: rowsAns[i] == rowsSum[i] }">{{ rowsAns[i] }} ({{ rowsSum[i] }})</td>
-        </tr>
-        <tr>
-            <td v-for="(num, i) in colsAns" :class="{ done: num == colsSum[i] }">
-                {{ num }} ({{ colsSum[i] }})
-            </td>
-        </tr>
-    </table>
+  </div>
+  <br /><br /><br />
+  <table>
+    <tr v-for="(row, i) in table">
+      <td
+        v-for="(cell, j) in row"
+        :class="{ selected: cell.selected }"
+        @click="table[i][j].selected = !cell.selected"
+      >
+        {{ cell.num }}
+      </td>
+      <td :class="{ done: rowsAns[i] == rowsSum[i] }">
+        {{ rowsAns[i] }} ({{ rowsSum[i] }})
+      </td>
+    </tr>
+    <tr>
+      <td v-for="(num, i) in colsAns" :class="{ done: num == colsSum[i] }">
+        {{ num }} ({{ colsSum[i] }})
+      </td>
+    </tr>
+  </table>
 </template>
